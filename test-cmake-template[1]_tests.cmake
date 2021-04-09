e7939c6bdbea89aa36f3ce8b11bab36cf5571b3d
RomanFihura <79198112+RomanFihura@users.noreply.github.com>
2021-04-09 16:38:53 +0300

    Delete test-cmake-template[1]_tests.cmake

--- test-cmake-template[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( doublestack.head /home/romanfihura/Documents/doublestackvector/test-cmake-template [==[--gtest_filter=doublestack.head]==] --gtest_also_run_disabled_tests)
-set_tests_properties( doublestack.head PROPERTIES WORKING_DIRECTORY /home/romanfihura/Documents/doublestackvector)
-set( test-cmake-template_TESTS doublestack.head)
