56f7d8a3a4b4181d85f0d2bf6f55b60ae9e55932
RomanFihura <79198112+RomanFihura@users.noreply.github.com>
2021-04-09 16:39:30 +0300

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/romanfihura/Documents/doublestackvector
-# Build directory: /home/romanfihura/Documents/doublestackvector
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/romanfihura/Documents/doublestackvector/test-doublestackvector[1]_include.cmake")
